0e2a7d356deac9115877532001feb0a2273d9c64
Dilip Kumar <dilipkumar.mattewada@gmail.com>
2021-12-15 16:44:44 +0530

    urls

--- src/Utils/globals.js
+++ src/Utils/globals.js
@@ -1,18 +1,19 @@
 const globalNames = 
 {
+
+  APIURL:'http://128.199.28.150:5000/api',
   APIURL : 'http://128.199.28.150:5000/api',
   // APIURL : 'http://127.0.0.1:5000/api',
-  RAWURL : 'http://127.0.0.1:5000',
+  RAWURL : 'http://128.199.28.150:5000',
   superadminlogout : function logout(e){
         e.preventDefault();
         localStorage.clear();
         window.location.href='/superadmin/login';
   },
-  USERFILES : 'http://127.0.0.1:5000/userfiles/',
-  BLOGFILES : 'http://127.0.0.1:5000/blog-files/',
-  PHARMACYFILES : 'http://127.0.0.1:5000/pharmacy-files/',
-  DIAGNOSTICSFILES : 'http://127.0.0.1:5000/lab-tests/',
-
+  USERFILES : 'http://128.199.28.150:5000/userfiles/',
+  BLOGFILES : 'http://128.199.28.150:5000/blog-files/',
+  PHARMACYFILES : 'http://128.199.28.150:5000/pharmacy-files/',
+  DIAGNOSTICSFILES : 'http://128.199.28.150:5000/lab-tests/',
   
 }
 export default globalNames;
